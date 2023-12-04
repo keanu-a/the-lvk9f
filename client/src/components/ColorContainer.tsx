dd017f1c2cbd91e47e548bf457549b2ff62cff54
keanu-a <keanucaloua@gmail.com>
2023-12-03 16:00:12 -0800

    Made Navbar responsive. Added phosphor icons

--- client/src/components/ColorContainer.tsx
+++ client/src/components/ColorContainer.tsx
@@ -1,27 +1,27 @@
 type Props = {
   children: React.ReactNode;
-  width?: string;
   bgColor?: string;
-  shift?: string;
+  width?: "full" | "half";
+  shift?: "right" | "left";
 };
 
 export default function ColorContainer({
   children,
-  width = "full",
   bgColor,
+  width = "full",
   shift = "right",
 }: Props) {
   return (
     <div
       className={`${
-        width === "full" ? "w-4/6" : "w-1/2"
+        width === "full" ? "max-w-6xl" : "w-1/2"
       } h-full flex flex-col justify-center mx-auto relative rounded-md ${bgColor}`}
     >
       {/* This is the white overlay - Over the blue background */}
       <div
         className={`absolute bg-main-white w-full h-full ${
           shift === "right" ? "ml-4" : "mr-4"
-        } mt-4 shadow-md rounded-md`}
+        } mt-4 shadow-lg rounded-md`}
       ></div>
 
       <div className="relative z-1">{children}</div>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import logo from "../assets/final-multi.jpg";
+import { List, X } from "@phosphor-icons/react";
 
 interface navText {
   to: string;
@@ -28,13 +30,46 @@ const navLinkText: navText[] = [
 ];
 
 export default function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <nav className="flex justify-between px-12 border-b-[6px] border-main-blue">
+    <nav className="flex justify-between items-center px-4 lg:px-12 border-b-[6px] border-main-blue relative">
       <NavLink to="/">
         <img src={logo} width={150} alt="The Las Vegas K-9 Foundation logo" />
       </NavLink>
 
-      <ul className="flex gap-12 justify-around items-center -lg:hidden">
+      <button
+        className="lg:hidden hover:text-main-blue h-fit"
+        onClick={() => setToggleMenu((prev) => !prev)}
+      >
+        {toggleMenu ? <X size={32} /> : <List size={32} />}
+      </button>
+
+      {/* Menu Nav Bar */}
+      <ul
+        className={`flex flex-col gap-8 p-8 w-full text-center absolute z-20 top-full right-0 
+                    shadow-lg rounded-b-lg bg-main-white lg:hidden ${
+                      !toggleMenu && "hidden"
+                    }`}
+      >
+        {navLinkText.map(({ to, text }, index) => (
+          <li key={index}>
+            <NavLink
+              to={to}
+              className={`text-xl font-medium ${
+                text === "Donate"
+                  ? "transition bg-main-blue hover:text-main-white p-4 rounded-lg"
+                  : "hover:text-main-blue"
+              }`}
+            >
+              {text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+
+      {/* Regular Nav Bar */}
+      <ul className="gap-12 justify-around items-center hidden lg:flex lg:flex-row">
         {navLinkText.map(({ to, text }, index) => (
           <li key={index}>
             <NavLink
