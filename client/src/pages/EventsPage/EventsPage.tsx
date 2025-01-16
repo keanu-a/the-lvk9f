8836852dcd0275a8b3cd761cc72cddd1b8d1780e
keanu-a <keanucaloua@gmail.com>
2025-01-15 19:57:22 -0800

    Added rally b-match event to home and events page

--- client/src/pages/EventsPage/EventsPage.tsx
+++ client/src/pages/EventsPage/EventsPage.tsx
@@ -1,10 +1,18 @@
 import Layout from '../../components/Layout';
 
-import yardSaleFlyer from '../../assets/events/yardsale.jpg';
+import yardSaleFlyer from '../../assets/events/yardsale-oct-24/yardsale.jpg';
+import rallyFlyer from '../../assets/events/rally-feb-1/rally.jpg';
 
 const IMAGE_SIZE = 700;
 
-const EVENTS = [
+const UPCOMING_EVENTS = [
+  {
+    eventImage: rallyFlyer,
+    altText: 'Rally B-Match flyer',
+  },
+];
+
+const PAST_EVENTS = [
   {
     eventImage: yardSaleFlyer,
     altText: '1st annual yard sale flyer',
@@ -14,13 +22,29 @@ const EVENTS = [
 export default function EventsPage() {
   return (
     <Layout bannerText="Events">
-      <h2 className="p-4 text-main-blue">{EVENTS.length} UPCOMING EVENT</h2>
+      <h2 className="p-4 text-main-blue">
+        {UPCOMING_EVENTS.length} UPCOMING EVENT
+      </h2>
 
-      {EVENTS.map((event, eventIndex) => (
+      {UPCOMING_EVENTS.map((event, eventIndex) => (
         <div key={eventIndex}>
           <img src={event.eventImage} alt={event.altText} width={IMAGE_SIZE} />
         </div>
       ))}
+
+      <h2 className="p-4 text-main-blue">PAST EVENTS</h2>
+
+      <div className="flex">
+        {PAST_EVENTS.map((event, eventIndex) => (
+          <div key={eventIndex}>
+            <img
+              src={event.eventImage}
+              alt={event.altText}
+              width={IMAGE_SIZE / 2}
+            />
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 }
