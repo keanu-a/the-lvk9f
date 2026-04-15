4811675bea60673beffcbe7b61cb992202408863
Keanu Aloua <71466438+keanu-a@users.noreply.github.com>
2026-04-15 15:46:38 -0700

    improve event page card ui (#40)

--- client/src/pages/EventsPage/EventsPage.tsx
+++ client/src/pages/EventsPage/EventsPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { MapPinLine } from '@phosphor-icons/react';
 
 import Layout from '../../components/Layout';
 import { events } from '../../data/events';
@@ -47,13 +48,14 @@ export default function EventsPage() {
 
   return (
     <Layout bannerText="Events">
+      {/* Filter buttons */}
       <div className="flex flex-wrap gap-2 p-2">
         {filters.map((filter) => (
           <button
             key={filter.value}
-            className={`px-4 py-2 rounded-full transition-all hover:bg-main-blue hover:bg-opacity-50 ${
+            className={`px-4 py-2 rounded-lg transition-all border-2 border-main-blue hover:bg-main-blue hover:bg-opacity-50 ${
               activeFilter === filter.value
-                ? 'bg-main-blue text-white'
+                ? 'bg-main-blue text-main-white'
                 : 'bg-gray-200 text-gray-700'
             }`}
             onClick={() => setActiveFilter(filter.value)}
@@ -63,52 +65,59 @@ export default function EventsPage() {
         ))}
       </div>
 
-      {filteredEvents.length === 0 ? (
-        <p className="text-center text-gray-500">More events coming soon!</p>
-      ) : (
-        <div className="flex flex-col gap-4 pb-4">
-          {filteredEvents.map((event, eventIndex) => (
-            <div key={eventIndex}>
-              {event.type === 'org' ? (
-                <div className="flex flex-col md:flex-row items-center gap-4 p-4 bg-main-gray rounded-lg shadow">
-                  <img src={event.image} alt={event.alt} width={IMAGE_SIZE} />
-                  <div>
-                    <h3 className="text-lg font-bold">{event.eventName}</h3>
-                    <p className="text-gray-600">{event.eventDetails}</p>
-                    <p className="text-sm text-gray-500">
-                      {formatDate(event.date)}
-                      {event.endDate && ` - ${formatDate(event.endDate)}`}
-                    </p>
+      <div className="w-screen flex justify-center">
+        {filteredEvents.length === 0 ? (
+          <p className="text-center text-gray-500">More events coming soon!</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 max-w-5xl">
+            {filteredEvents.map((event, eventIndex) => (
+              <div key={eventIndex}>
+                {event.type === 'org' ? (
+                  <div className="flex flex-col md:flex-row items-center gap-4 p-4 bg-main-gray rounded-lg shadow">
+                    <img src={event.image} alt={event.alt} width={IMAGE_SIZE} />
+                    <div>
+                      <h3 className="text-lg font-bold">{event.eventName}</h3>
+                      <p className="text-gray-600">{event.eventDetails}</p>
+                      <p className="text-sm text-gray-500">
+                        {formatDate(event.date)}
+                        {event.endDate && ` - ${formatDate(event.endDate)}`}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ) : (
-                <div className="flex flex-col md:flex-row gap-4 p-4 bg-main-gray rounded-lg shadow">
-                  <div>
-                    <h3 className="text-lg font-bold">{event.organizer}</h3>
+                ) : (
+                  <div className="flex flex-col gap-2 p-4 bg-main-gray rounded-lg shadow+">
+                    <div>
+                      <p className="text-sm text-main-blue-darker md:text-base">
+                        {formatDate(event.date)}
+                        {event.endDate && ` - ${formatDate(event.endDate)}`}
+                      </p>
+                      <h3 className="text-lg font-bold">{event.organizer}</h3>
+                      <p className="flex items-center gap-1">
+                        <MapPinLine />
+                        {event.location}
+                      </p>
+                    </div>
                     <p className="text-gray-600">
                       {event.disciplines.join(', ')}
                     </p>
-                    <p className="text-sm text-gray-500">
-                      {formatDate(event.date)}
-                      {event.endDate && ` - ${formatDate(event.endDate)}`}
-                    </p>
                     {event.website && (
-                      <a
-                        href={event.website}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-main-blue hover:underline"
-                      >
-                        Visit Website
-                      </a>
+                      <button className="bg-main-blue text-main-white transition-all mt-1 p-2 rounded-lg hover:bg-main-blue/80 w-full md:w-1/3">
+                        <a
+                          href={event.website}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Visit Website
+                        </a>
+                      </button>
                     )}
                   </div>
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-      )}
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
 
       {/* <h2 className="p-4 text-main-blue">
         {UPCOMING_EVENTS.length} UPCOMING EVENT
